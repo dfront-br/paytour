37627fd7acd17e77eca88a5218c0353c37941993
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-03-23 01:08:48 -0300

    [Route] Criação das rotas endpoints: - / (redirect) - /jobs (get, post)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JobController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/jobs');
+});
+
+Route::prefix('jobs')->group(function () {
+    Route::get('/', [JobController::class, 'index']);
+    Route::post('/', [JobController::class, 'store']);
 });
